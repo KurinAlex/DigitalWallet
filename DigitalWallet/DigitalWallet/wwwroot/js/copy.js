51c4bbf63b7dfaf426241b5e11ef90ee7e149d7d
Oleksii Kurin <olexkurin@gmail.com>
2024-05-29 09:58:12 +0300

    Center the content and add ability to copy wallet id

---
+++ DigitalWallet/DigitalWallet/wwwroot/js/copy.js
@@ -0,0 +1,20 @@
+﻿toastr.options = {
+    "closeButton": true,
+    "positionClass": "toast-top-center"
+};
+
+walletId = document.getElementById("walletId");
+
+walletId.addEventListener("click",
+    () => {
+        if (window.isSecureContext) {
+            var text = walletId.textContent;
+            navigator.clipboard.writeText(text).then(() => {
+                console.log("Text copied to clipboard:", text);
+                toastr.success("Wallet ID copied!");
+            }).catch(err => {
+                console.error("Failed to copy text: ", err);
+                toastr.error("Failed to copy text");
+            });
+        }
+    });

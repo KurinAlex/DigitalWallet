6d153f32ba94982150b7f48ff0f17e1639f35bca
Oleksii Kurin <olexkurin@gmail.com>
2024-06-03 01:02:48 +0300

    Add transactions table view page

---
+++ DigitalWallet/DigitalWallet/Pages/Transactions.cshtml.cs
@@ -0,0 +1,23 @@
+﻿using DigitalWallet.Models;
+using DigitalWallet.Services;
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Identity;
+
+namespace DigitalWallet.Pages
+{
+    public class TransactionsModel(
+        UserManager<Client> clientManager,
+        WalletManager walletManager,
+        TransactionManager transactionManager)
+        : PageModel
+    {
+        public async Task<IActionResult> OnGetTransactionsAsync()
+        {
+            var client = await clientManager.GetUserAsync(User);
+            var wallet = await walletManager.FindByClientAsync(client);
+            return new JsonResult(await transactionManager.GetTransactionsAsync(wallet));
+        }
+    }
+}
---
+++ DigitalWallet/DigitalWallet/wwwroot/js/datatable.js
@@ -0,0 +1,42 @@
+﻿$(document).ready(function () {
+    $('#table').DataTable({
+        ajax: {
+            url: 'Transactions?handler=Transactions',
+            dataSrc: ''
+        },
+        columns: [
+            {
+                data: 'time'
+            },
+            {
+                data: 'amount'
+            },
+            {
+                data: 'status',
+                render: function (data, type) {
+                    switch (data) {
+                        case 0:
+                            return getIcon('In Progress', 'bi-arrow-repeat text-warning');
+                        case 1:
+                            return getIcon('Failed', 'bi-dash-circle text-danger');
+                        case 2:
+                            return getIcon('Success', 'bi-check2-circle text-success');
+                        default:
+                            return getIcon('Unknown', 'bi-question-circle text-info');
+                    }
+                }
+            },
+            {
+                data: 'id',
+                sortable: false,
+                render: function (data, type) {
+                    return `<a href="/TransactionDetails?id=${data}">Details</a>`;
+                }
+            }
+        ]
+    });
+});
+
+function getIcon(title, iconClass) {
+    return `<i title="${title}" class="bi ${iconClass}"></i>`;
+}

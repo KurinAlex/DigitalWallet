8b1559d0584cfad2a346b4d18484547d18c9cfaf
Oleksii Kurin <olexkurin@gmail.com>
2024-06-04 00:08:09 +0300

    Add styling to datatable

--- DigitalWallet/DigitalWallet/wwwroot/js/datatable.js
+++ DigitalWallet/DigitalWallet/wwwroot/js/datatable.js
@@ -1,41 +1,59 @@
 ﻿$(document).ready(function () {
-    $('#table').DataTable({
+    let table = $('#table').DataTable({
         ajax: {
             url: 'Transactions?handler=Transactions',
             dataSrc: ''
         },
+        columnDefs: [
+            {
+                targets: "_all",
+                className: 'dt-body-left dt-head-left'
+            }
+        ],
         columns: [
             {
-                data: 'end'
+                data: 'subject'
             },
             {
-                data: 'amount'
+                data: 'time'
+            },
+            {
+                data: 'amount',
+                render: function (data, type) {
+                    return data < 0 ? getAmountSpan('danger', data) : getAmountSpan('success', data);
+                }
             },
             {
                 data: 'status',
                 render: function (data, type) {
                     switch (data) {
-                        case 0:
+                        case 'InProgress':
                             return getIcon('In Progress', 'bi-arrow-repeat text-warning');
-                        case 1:
+                        case 'Failed':
                             return getIcon('Failed', 'bi-dash-circle text-danger');
-                        case 2:
+                        case 'Succeeded':
                             return getIcon('Success', 'bi-check2-circle text-success');
                         default:
                             return getIcon('Unknown', 'bi-question-circle text-info');
                     }
                 }
-            },
-            {
-                data: 'id',
-                sortable: false,
-                render: function (data, type) {
-                    return `<a href="/TransactionDetails?id=${data}">Details</a>`;
-                }
             }
-        ]
+        ],
+        order: {
+            idx: 1,
+            dir: 'desc'
+        }
+    });
+
+    table.on('click', 'tbody tr', function () {
+        let data = table.row(this).data();
+        location.href = `/TransactionDetails?id=${data['id']}`;
     });
 });
+
+function getAmountSpan(textStyle, amount) {
+    return `<span class="text-${textStyle}">${amount}$</span>`;
+}
 
 function getIcon(title, iconClass) {
     return `<i title="${title}" class="bi ${iconClass}"></i>`;

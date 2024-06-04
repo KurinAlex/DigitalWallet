5b2e3e7ce6fc01520ad1e5d8b9f559efbc769c13
Oleksii Kurin <olexkurin@gmail.com>
2024-06-04 16:37:11 +0300

    Reconfigure Toastr notifications

---
+++ DigitalWallet/DigitalWallet/wwwroot/js/toastr.js
@@ -0,0 +1,19 @@
+﻿toastr.options = {
+    "closeButton": true,
+    "positionClass": 'toast-top-center'
+};
+
+$('.copy-content').each(function() {
+    $(this).on('click',
+        () => {
+            const text = $(this).prop('innerText');
+            navigator.clipboard.writeText(text).then(() => {
+                console.log('Text copied to clipboard:', text);
+                toastr.success('Text copied!');
+            }).catch(err => {
+                console.error('Failed to copy text: ', err);
+                toastr.error('Failed to copy text');
+            });
+        }
+    );
+});
